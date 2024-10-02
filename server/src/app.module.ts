9b4a2ef3caa0c58715579bb1bf4f52be12565903
Viktor Rura <ruravictor@gmail.com>
2024-10-02 19:12:09 +0100

    Add database module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
